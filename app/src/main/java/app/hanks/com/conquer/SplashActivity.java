5bbb872e4731db60135d97197e4c7a264362c55b
hanks <1161745215@qq.com>
2015-05-17 15:55:03 +0800

    project init

---
+++ app/src/main/java/app/hanks/com/conquer/SplashActivity.java
@@ -0,0 +1,15 @@
+package app.hanks.com.conquer;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class SplashActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+}
